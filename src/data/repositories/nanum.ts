79b28f8bb609b41ba6f30ff430be99a991367bb4
JeongKyungSeo <jerion7474@gmail.com>
2019-06-02 22:43:21 +0900

    Reboot

--- src/data/repositories/nanum.ts
+++ src/data/repositories/nanum.ts
@@ -3,7 +3,6 @@ import { Collection } from "mongodb";
 import { NanumModel } from "../models/nanum";
 import { NanumRepository } from "../../services/repositoryInterfaces/nanum";
 import MongoConnection from "../mongo";
-import { GetNanumListCondition } from "../../types/getNanumTypes";
 import { PatchOwnNanumCondition } from "../../types/patchNanumTypes";
 import { MONGODB_NANUM_COLLECTION } from "../../constant/mongo";
 
@@ -15,7 +14,7 @@ export class MongoNanumRepository implements NanumRepository {
     this.collection = MongoConnection.getCollection(MONGODB_NANUM_COLLECTION);
   }
   
-  public async find(condition: GetNanumListCondition): Promise<NanumModel[]> {
+  public async find(condition: object): Promise<NanumModel[]> {
     return await this.collection.find(condition).toArray();
   }
 
--- src/presentaion/routers/nanum/changeStarState.ts
+++ src/presentaion/routers/nanum/changeStarState.ts
@@ -1,15 +1,8 @@
 import { Request, Response } from "express";
 
-import { MongoNanumRepository } from "../../../data/repositories/nanum";
-import changeStarState from "../../../services/nanum/changeStarState";
-
 
 const changeStarStateHandler = async (req: Request, res: Response) => {
-  const nanumId: string = req.params.nanumId;
-  const apartmentId: string = req.params.apartmentId;
-  const repository = new MongoNanumRepository();
 
-  await changeStarState(repository, nanumId, apartmentId);
 };
 
 export default changeStarStateHandler;
