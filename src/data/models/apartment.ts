c74c1a0e6758ae9916585030ec8c6038f39a59e7
JeongKyungSeo <jerion7474@gmail.com>
2019-06-03 14:21:58 +0900

    Reflect review

--- src/data/models/apartment.ts
+++ src/data/models/apartment.ts
@@ -1,6 +1,8 @@
+type NanumId = string;
+
 export interface ApartmentModel {
   id: string;
-  stared: string[];
-  joined: string[];
-  raised: string[];
+  stared: NanumId[];
+  joined: NanumId[];
+  raised: NanumId[];
 }
