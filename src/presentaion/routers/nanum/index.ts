d5038593c21388dad3d8d35d97cd52ba850fca32
HubCodes <krlrhkstk12@gmail.com>
2019-06-04 22:56:57 +0900

    apply multer dependency

--- src/presentaion/routers/nanum/index.ts
+++ src/presentaion/routers/nanum/index.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
+import multer from "multer";
 
 import createNanumHandler from "./createNanumHandler";
 import getNanumDetailHandler from "./getNanumDetailHandler";
 import getRaisedNanumListHandler from "./getRaisedNanumListHandler";
-
+import uploadImageHandler from "./uploadImage";
 
 const NanumRouter: Router = Router();
+const multerStorage = multer.memoryStorage();
 
 NanumRouter.get("/:apartmentId/raised", getRaisedNanumListHandler);
 NanumRouter.post("/:apartmentId/raised", createNanumHandler);
 NanumRouter.get("/:nanumId", getNanumDetailHandler);
+NanumRouter.post("/image", multer({ storage: multerStorage }).single(), uploadImageHandler);
 
 export default NanumRouter;
