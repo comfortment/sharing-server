a99d5b28ea3a6750de5f0c8e1a3f5b5b8fa292fd
HubCodes <krlrhkstk12@gmail.com>
2019-06-05 10:28:39 +0900

    implement updateOne for NanumRepository

--- src/data/models/nanum.ts
+++ src/data/models/nanum.ts
@@ -1,9 +1,11 @@
 import { ApartmentId, NanumWithoutApartment } from "../../types/nanum";
 import { NanumId } from "../../entities/Nanum";
-
+import { Omit } from "../../types/Omit";
 
 // MongoDb Collection _ "nanum"
 export interface NanumModel extends NanumWithoutApartment {
   apartmentId: ApartmentId;
   nanumId: NanumId;
 }
+
+export type NanumModelUpdateQuery = Partial<Omit<NanumModel, "apartmentId">>;
