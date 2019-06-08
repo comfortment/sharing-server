a40a62d89bc8bcbfa9883841117fde4adbe6296e
HubCodes <krlrhkstk12@gmail.com>
2019-06-08 23:12:54 +0900

    considering api gateway

--- src/app.ts
+++ src/app.ts
@@ -5,20 +5,19 @@ import http from "http";
 import NanumRouter from "./presentaion/routers/nanum";
 import morgan from "morgan";
 
-
 class App {
   private app: express.Express;
   private server: http.Server;
-  
+
   public constructor() {
     this.app = express();
     this.server = http.createServer(this.app);
-    
+
     this.app.use(morgan("combined"));
     this.app.use(express.json());
     this.app.use(helmet());
 
-    this.app.use("/nanum", NanumRouter);
+    this.app.use("/", NanumRouter);
   }
 
   public listen(port?: number, hostname?: string) {
