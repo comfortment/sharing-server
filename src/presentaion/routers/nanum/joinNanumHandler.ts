0b0c7e3d083dd724c0e241ad220344dc5347eb66
HubCodes <krlrhkstk12@gmail.com>
2019-06-10 08:14:35 +0900

    hotfix: joinNanum

--- src/presentaion/routers/nanum/joinNanumHandler.ts
+++ src/presentaion/routers/nanum/joinNanumHandler.ts
@@ -3,19 +3,23 @@ import { ApartmentId } from "../../../types/nanum";
 import { NanumId } from "../../../entities/Nanum";
 import { MongoOwnApartmentRepository } from "../../../data/repositories/mongoOwnApartment";
 import joinNanum from "../../../services/nanum/joinNanum";
-
+import { NonExistApartmentError } from "../../../exception";
 
 const joinNanumHandler = async (req: Request, res: Response) => {
   const apartmentId: ApartmentId = req.params.apartmentId;
   const nanumId: NanumId = req.params.nanumId;
   const mongoOwnApartmentRepository = new MongoOwnApartmentRepository();
-  
+
   try {
     await joinNanum(mongoOwnApartmentRepository, nanumId, apartmentId);
 
     res.status(200).send();
   } catch (e) {
-    res.status(500).send();
+    if (e instanceof NonExistApartmentError) {
+      res.status(404).send();
+    } else {
+      res.status(500).send();
+    }
   }
 };
 
