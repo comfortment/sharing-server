9a2b3031f1c064d0d067b11365d8154a154e3eba
HubCodes <krlrhkstk12@gmail.com>
2019-06-10 10:21:27 +0900

    hotfix: upload image

--- src/services/nanum/uploadImage.ts
+++ src/services/nanum/uploadImage.ts
@@ -2,10 +2,11 @@ import { S3_BUCKET } from "../../constant/aws";
 import uuid from "uuid/v4";
 
 const uploadImage = async (image: Buffer, s3: AWS.S3): Promise<string> => {
+  const decodedImage = Buffer.from(image.toString("ascii"), "base64");
   const fileName = uuid();
   const { Location } = await s3
     .upload({
-      Body: image,
+      Body: decodedImage,
       Bucket: S3_BUCKET,
       Key: fileName,
       ContentType: "application/octet-stream",
