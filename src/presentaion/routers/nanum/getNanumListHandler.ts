34dcc4dfa6f0b732526540c2d7df1a154a5c8a77
HubCodes <krlrhkstk12@gmail.com>
2019-06-10 15:50:16 +0900

    hotfix: getNanumList

--- src/presentaion/routers/nanum/getNanumListHandler.ts
+++ src/presentaion/routers/nanum/getNanumListHandler.ts
@@ -5,8 +5,8 @@ import getNanumList from "../../../services/nanum/getNanumList";
 import { NanumType } from "../../../entities/Nanum";
 
 const getNanumListHandler = async (req: Request, res: Response) => {
-  const rawType: NanumType | undefined = req.body.type;
-  const rawExpiry: string | undefined = req.body.expiry;
+  const rawType: NanumType | undefined = req.query.type;
+  const rawExpiry: string | undefined = req.query.expiry;
 
   const type = rawType;
   const expiry = Number(rawExpiry) || undefined;
